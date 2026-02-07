76ab97e87a878ef2dd68b9561224585644c85571
joao-paulo-hoff-marcos <jaopaulohoffm@gmail.com>
2026-02-07 11:38:11 -0300

    resolvendo problema 1006

---
+++ src/p1006.rs
@@ -0,0 +1,29 @@
+
+
+//Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+//Entrada
+
+//O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
+//Saída
+
+//Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+
+fn get_num() -> f64 {
+    use std::io;
+    let mut x1 = String::new();
+    io::stdin().read_line(&mut x1).expect("a string não deu certo");
+    let a: f64 = x1.trim().parse().expect("a string não virou um numero");
+    return a;
+}
+
+
+pub fn main() {
+    let a = get_num();
+    let b = get_num();
+    let c = get_num();
+
+    let x: f64 = (a*2.0 + b*3.0+ c*5.0) / 10.0;
+
+    println!("MEDIA = {:.1}", x);
+}
